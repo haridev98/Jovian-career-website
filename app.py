e6450d7cf82f4b3b852c3671ca5c1eced33f103c
Hari Dev <haridev5041998@gmail.com>
2022-12-04 12:28:25 +0000

    added the flask first route

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def Hell_world():
+ # print( "hellow world")
+  return "World------------- Hello"
+
+if __name__=="__main__":
+  app.run('0.0.0.0',debug = True)
+
