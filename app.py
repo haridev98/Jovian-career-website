793837c5e623dc45bf2f0c84d0c777f5e44f825d
Hari Dev <haridev5041998@gmail.com>
2022-12-04 12:49:13 +0000

    added templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def Hell_world():
  # print( "hellow world")
-  return "World------------- Hello"
+  return render_template('home.html')
 
 if __name__=="__main__":
   app.run('0.0.0.0',debug = True)
